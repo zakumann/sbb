b08531b3cb329a1314998b508dad242f79c3c7fa
zakumann <samuel742able@gmail.com>
2023-09-24 11:55:57 +0900

    2-10

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+	
 	@GetMapping("/sbb")
 	@ResponseBody
 	public String index() {
 		return "Welcome to the sbb.";
+	}
+	
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list";
 	}
 }
