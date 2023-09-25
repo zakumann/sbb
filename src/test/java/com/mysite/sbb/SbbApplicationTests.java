c9283f40de7ec7745504968fa709439d10645373
zakumann <samuel742able@gmail.com>
2023-09-25 13:34:31 +0900

    3-04

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,37 +1,23 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.mysite.sbb.answer.Answer;
-import com.mysite.sbb.question.Question;
-import com.mysite.sbb.question.QuestionRepository;
-
+import com.mysite.sbb.question.QuestionService;
 
 @SpringBootTest
-class SbbApplicationTests {
-
-	@Autowired
-	private QuestionRepository questionRepository;
+class sbbApplicationTests{
 	
-	@Transactional
+	@Autowired
+	private QuestionService questionService;
+	
 	@Test
 	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-		
-		List<Answer> answerList = q.getAnswerList();
-		
-		assertEquals(1, answerList.size());
-		assertEquals("Yes It's generate automatically", answerList.get(0).getContent());
+		for(int i = 1; i <= 300; i++) {
+			String subject = String.format("Test data:[%03d]", i);
+			String content = "null";
+			this.questionService.create(subject, content);
+		}
 	}
 }
