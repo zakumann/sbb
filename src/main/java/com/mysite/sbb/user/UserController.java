8ade3c1a3aa88c90afe95c0f18ae6d57476e7bb6
zakumann <samuel742able@gmail.com>
2023-09-26 22:43:32 +0900

    3-07

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -14,39 +14,44 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
-	private final UserService userService;
-	
-	@GetMapping("/signup")
-	public String signup(UserCreateForm userCreateForm) {
-		return "signup_form";
-	}
-	
-	@PostMapping("/signup")
+
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "signup_form";
         }
-		
+
         if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
             bindingResult.rejectValue("password2", "passwordInCorrect", 
-					"Both passwords are not matched.");
+                    "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
-		}
-        
-        try {
-        	userService.create(userCreateForm.getUsername(),
-				userCreateForm.getEmail(), userCreateForm.getPassword1());
-        }catch(DataIntegrityViolationException e) {
-        	e.printStackTrace();
-        	bindingResult.reject("signupFailed", "Already registred User.");
-        	return "signup_form";
-        }catch(Exception e) {
-        	e.printStackTrace();
-        	bindingResult.reject("signupFailed", e.getMessage());
-        	return "signup_form";
         }
-        
+
+        try {
+            userService.create(userCreateForm.getUsername(), 
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
         return "redirect:/";
-	}
+    }
+    
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
 }
